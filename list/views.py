2c1d0529dadd896ba9fdb8c5c136e7dbdc574e8f
utkarsh <utkjaiswal58@gmail.com>
2023-07-04 18:30:44 +0530

    tasks being displayed

--- list/views.py
+++ list/views.py
@@ -3,16 +3,23 @@ from django.shortcuts import render
 from list.models import Tasks
 
 def index(request):
-    return render(request,"index.html")
+    all_tasks = Tasks.objects.all()
+    context = {
+        'all_tasks':all_tasks
+    }
+    return render(request,"index.html",context)
 
 
 def create(request):
     if request.method == "POST":
         title = request.POST.get("title")
         desc = request.POST.get("desc")
-        print("Title is ",title)
-        
         task = Tasks(title=title, desc=desc)
         task.save()
-        return render(request,"index.html")
+        all_tasks = Tasks.objects.all()
+        context = {
+            'all_tasks':all_tasks
+        }
+        
+        return render(request,"index.html",context)
     return render(request,"create.html")
