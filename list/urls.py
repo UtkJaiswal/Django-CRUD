8131dec7ee0c034c004f6a5afe721c7b2af739fa
utkarsh <utkjaiswal58@gmail.com>
2023-07-05 01:08:03 +0530

    edit and delete functionality added

--- list/urls.py
+++ list/urls.py
@@ -5,5 +5,8 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("create/", views.create, name="create"),
+    path("edit/<int:id>/", views.edit, name="edit"),
+    path("save/<int:id>/", views.save, name="save"),
+    path("delete/<int:id>/", views.delete, name="delete"),
 
 ]
--- list/views.py
+++ list/views.py
@@ -23,3 +23,45 @@ def create(request):
         
         return render(request,"index.html",context)
     return render(request,"create.html")
+
+def edit(request,id):
+    curr_task = Tasks.objects.get(id=id)
+    context = {
+            'curr_task':curr_task
+        }
+    return render(request,"edit.html",context)
+
+def save(request,id):
+    curr_task_title = request.POST.get("title")
+    curr_task_desc = request.POST.get("desc")
+
+    curr_task=Tasks.objects.get(id=id)
+
+    curr_task.title=curr_task_title
+    curr_task.desc=curr_task_desc
+
+    curr_task.save()
+
+    all_tasks = Tasks.objects.all()
+
+    context = {
+            'all_tasks':all_tasks
+        }
+    
+    return render(request,"index.html",context)
+
+def delete(request,id):
+    
+
+    curr_task=Tasks.objects.get(id=id)
+
+
+    curr_task.delete()
+
+    all_tasks = Tasks.objects.all()
+
+    context = {
+            'all_tasks':all_tasks
+        }
+    
+    return render(request,"index.html",context)
