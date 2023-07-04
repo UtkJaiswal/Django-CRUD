42ad09cc558017ae4f577ed001c5315df6b9ee17
utkarsh <utkjaiswal58@gmail.com>
2023-07-04 18:15:25 +0530

    task addition using form functionality added

--- list/admin.py
+++ list/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
+from list.models import Tasks
+
+admin.site.register(Tasks)
--- list/views.py
+++ list/views.py
@@ -1,10 +1,18 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from list.models import Tasks
 
 def index(request):
     return render(request,"index.html")
 
 
 def create(request):
+    if request.method == "POST":
+        title = request.POST.get("title")
+        desc = request.POST.get("desc")
+        print("Title is ",title)
+        
+        task = Tasks(title=title, desc=desc)
+        task.save()
+        return render(request,"index.html")
     return render(request,"create.html")
